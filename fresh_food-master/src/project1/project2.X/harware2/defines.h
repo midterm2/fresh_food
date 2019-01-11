7a90658b491404f7e0439d61ef8a0e534001c2a9
1105104230 <45057687+1105104230@users.noreply.github.com>
2019-01-11 22:59:11 +0800

    Add files via upload

---
+++ fresh_food-master/src/project1/project2.X/harware2/defines.h
@@ -0,0 +1,13 @@
+
+#ifndef _DEFINES_H
+#define	_DEFINES_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+typedef unsigned int u16;
+typedef unsigned char u8;
+typedef unsigned char bool;
+#define true 1
+#define false 0
+#define RX_BUF_MAX_LEN 60
+#endif	/* XC_HEADER_TEMPLATE_H */
+
